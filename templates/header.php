322fd153d6a8264658d5c186ca179d6f8b40ed78
Fomichev Roman <roman_f_qschool@qsoft.ru>
2023-11-21 08:13:55 +0000

    Исправление шаблонов авторизации и пользовательского меню

--- templates/header.php
+++ templates/header.php
@@ -28,23 +28,9 @@ if (isAuthorized()) {
                 </div>
                 <div>
                     <?php if (! $isAuthorized) {
-                        includeTemplate('authorizeMenu.php', [
-                            'regField' => 'Регистрация',
-                            'authField' => 'Авторизация',
-                            'regHrefFielf' => '/register',
-                            'authHrefFielf' => '/login',
-                            'authImage' => 'M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z',
-                            'regImage' => 'M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z',
-                        ]);
+                        includeTemplate('authorizeMenu.php', []);
                     } else {
-                        includeTemplate('authorizeMenu.php', [
-                            'regField' => $userEmail,
-                            'authField' => 'Выйти',
-                            'regHrefFielf' => '#',
-                            'authHrefFielf' => '?logout=yes',
-                            'authImage' => 'M4,12a1,1,0,0,0,1,1h7.59l-2.3,2.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l4-4a1,1,0,0,0,.21-.33,1,1,0,0,0,0-.76,1,1,0,0,0-.21-.33l-4-4a1,1,0,1,0-1.42,1.42L12.59,11H5A1,1,0,0,0,4,12ZM17,2H7A3,3,0,0,0,4,5V8A1,1,0,0,0,6,8V5A1,1,0,0,1,7,4H17a1,1,0,0,1,1,1V19a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V16a1,1,0,0,0-2,0v3a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V5A3,3,0,0,0,17,2',
-                            'regImage' => 'M15.71,12.71a6,6,0,1,0-7.42,0,10,10,0,0,0-6.22,8.18,1,1,0,0,0,2,.22,8,8,0,0,1,15.9,0,1,1,0,0,0,1,.89h.11a1,1,0,0,0,.88-1.1A10,10,0,0,0,15.71,12.71ZM12,12a4,4,0,1,1,4-4A4,4,0,0,1,12,12Z',
-                        ]);
+                        includeTemplate('userMenu.php', [ 'userField' => $userEmail ]);
                     }?>  
                 </div>
             </div>
---
+++ templates/userMenu.php
@@ -0,0 +1,18 @@
+<ul class="flex justify-center sm:justify-end items-center space-x-8 text-sm">
+    <li>
+        <a class="text-gray-500 hover:text-orange" href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.71,12.71a6,6,0,1,0-7.42,0,10,10,0,0,0-6.22,8.18,1,1,0,0,0,2,.22,8,8,0,0,1,15.9,0,1,1,0,0,0,1,.89h.11a1,1,0,0,0,.88-1.1A10,10,0,0,0,15.71,12.71ZM12,12a4,4,0,1,1,4-4A4,4,0,0,1,12,12Z" />
+            </svg>
+            <?=$userField?>
+        </a>
+    </li>
+    <li>
+        <a class="text-gray-500 hover:text-orange" href="?logout=yes">
+            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M4,12a1,1,0,0,0,1,1h7.59l-2.3,2.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l4-4a1,1,0,0,0,.21-.33,1,1,0,0,0,0-.76,1,1,0,0,0-.21-.33l-4-4a1,1,0,1,0-1.42,1.42L12.59,11H5A1,1,0,0,0,4,12ZM17,2H7A3,3,0,0,0,4,5V8A1,1,0,0,0,6,8V5A1,1,0,0,1,7,4H17a1,1,0,0,1,1,1V19a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V16a1,1,0,0,0-2,0v3a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V5A3,3,0,0,0,17,2" clip-rule="evenodd" />
+            </svg>
+            Выйти
+        </a>
+    </li>
+</ul>
